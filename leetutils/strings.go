2d9e76178a101aec6d851dfcb7bf5ef4e398c316
arithbar <arithbar@outlook.com>
2020-04-03 13:15:53 +0800

    feature: leetutils

--- leetutils/strings.go
+++ leetutils/strings.go
@@ -6,4 +6,5 @@ func Reverse(s string) string {
 	for head, tail := 0, len(runes)-1; head < tail; head, tail := head+1, tail-1 {
 		runes[head], runes[tail] = runes[tail], runes[head]
 	}
+	return string(runes)
 }
