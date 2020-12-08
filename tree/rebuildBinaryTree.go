f409aa220c660a1f5d182f120716ee31934af79d
fusidic <arithbar@gmail.com>
2020-12-08 11:15:27 +0800

    medium

--- tree/rebuildBinaryTree.go
+++ tree/rebuildBinaryTree.go
@@ -24,6 +24,7 @@ func buildTree(preorder []int, inorder []int) *structures.TreeNode {
 			// 与 inorder 中不一致(非父节点)，相邻后点为左子树
 			cur.Left = &structures.TreeNode{}
 			cur.Left.Val = preorder[i]
+			// 将该左子树存入栈中，便于之后确定其右子树
 			stack.PushBack(cur)
 			cur = cur.Left
 		} else {
@@ -41,3 +42,44 @@ func buildTree(preorder []int, inorder []int) *structures.TreeNode {
 	}
 	return root
 }
+
+// Java Code
+
+// public TreeNode buildTree(int[] preorder, int[] inorder) {
+//     if (preorder.length == 0) {
+//         return null;
+//     }
+//     Stack<TreeNode> roots = new Stack<TreeNode>();
+//     int pre = 0;
+//     int in = 0;
+//     //先序遍历第一个值作为根节点
+//     TreeNode curRoot = new TreeNode(preorder[pre]);
+//     TreeNode root = curRoot;
+//     roots.push(curRoot);
+//     pre++;
+//     //遍历前序遍历的数组
+//     while (pre < preorder.length) {
+//         //出现了当前节点的值和中序遍历数组的值相等，寻找是谁的右子树
+//         if (curRoot.val == inorder[in]) {
+//             //每次进行出栈，实现倒着遍历
+//             while (!roots.isEmpty() && roots.peek().val == inorder[in]) {
+//                 curRoot = roots.peek();
+//                 roots.pop();
+//                 in++;
+//             }
+//             //设为当前的右孩子
+//             curRoot.right = new TreeNode(preorder[pre]);
+//             //更新 curRoot
+//             curRoot = curRoot.right;
+//             roots.push(curRoot);
+//             pre++;
+//         } else {
+//             //否则的话就一直作为左子树
+//             curRoot.left = new TreeNode(preorder[pre]);
+//             curRoot = curRoot.left;
+//             roots.push(curRoot);
+//             pre++;
+//         }
+//     }
+//     return root;
+// }
